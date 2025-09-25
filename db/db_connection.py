e294b05355af5062d12f2d80cb6a1ed2b2349f14
salmabenslimane <salma.benslimane@centrale-casablanca.ma>
2025-09-25 16:15:51 +0100

    update db_connection

--- db/db_connection.py
+++ db/db_connection.py
@@ -6,4 +6,4 @@ DB_FILE = os.path.join(os.path.dirname(__file__), "raw_data.duckdb")
 def get_connection():
     """Return a DuckDB connection."""
     conn = duckdb.connect(DB_FILE)
-     return conn   
+    return conn   
